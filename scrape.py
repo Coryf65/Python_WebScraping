448e1355696830c563f8c4e141e437089aaf7ae0
Coryf65 <coryf65@gmail.com>
2020-02-16 22:16:00 -0600

    added comments

--- scrape.py
+++ scrape.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup
 
 url = 'http://quotes.toscrape.com/'
 
+# picking our site
 response = requests.get(url)
 
+# picking our parser
 soup = BeautifulSoup(response.text, 'lxml')
 
+# seeing the html
 print(soup)
