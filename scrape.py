f9e90e76cbde4dc61543bdf014e074c820477f9d
Coryf65 <coryf65@gmail.com>
2020-02-21 17:09:56 -0600

    scrape the pag getting quotes and authors names.

--- scrape.py
+++ scrape.py
@@ -10,5 +10,19 @@ response = requests.get(url)
 # picking our parser
 soup = BeautifulSoup(response.text, 'lxml')
 
-# seeing the html
-print(soup)
+# getting the span tags only
+quotes = soup.find_all('span', class_='text')
+
+# getting all the authors as well
+authors = soup.find_all('small', class_='author')
+
+# getting the tag for quotes
+tags = soup.find_all('div', class_='tags')
+
+# now printing the authors and quotes
+for i in range(0, len(quotes)):
+    print(quotes[i].text)
+    print(authors[i].text)
+    quote_tags = tags[i].find_all('a', class_='tag')
+    for quote_tag in quote_tags:
+        print(quote_tag.text)
